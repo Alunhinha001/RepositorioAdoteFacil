f23a23c782111c97b348199b9c00cb4bbc7f44d8
Sego <isaacenzo126@gmail.com>
2025-11-03 14:27:07 -0300

    Create conexao.php

---
+++ PHP/conexao.php
@@ -0,0 +1,15 @@
+<?php
+    $host = 'localhost';
+    $dbname = 'cadastropet';
+    $username = 'root';
+    $password = 'root';
+
+    try{
+        $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+
+        $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        echo "Conexão com o banco de dados realizado com sucesso!";
+    } catch (PDOException $e) {
+        echo 'Erro de conexão: ' . $e->getMessage();
+    }
+?>
