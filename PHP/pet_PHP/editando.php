a7fad74fb57d1140037182220902fc9d05b67e4a
Karolynnexz <karolynnepaulo70@gmail.com>
2025-11-06 16:58:38 -0300

    Concluído 2

--- PHP/pet_PHP/editando.php
+++ PHP/pet_PHP/editando.php
@@ -43,6 +43,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="editar.css">
     <title>Editar Usuario</title>
 </head>
 <body>
