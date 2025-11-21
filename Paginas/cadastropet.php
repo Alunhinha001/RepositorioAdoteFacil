d5c38103803730ca941164b74ee3148cf73f2cd8
Superchoque014 <jfrazao231@gmail.com>
2025-11-21 17:03:32 -0300

    junção

---
+++ Paginas/cadastropet.php
@@ -0,0 +1,177 @@
+<?php
+session_start();
+include ('../PHP/conexao.php');
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Adote Fácil - Cadastro de Pet</title>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet" />
+  <link rel="stylesheet" href="../CSS/padrao.css"/>
+  <link rel="stylesheet" href="../CSS/cadastropet.css"/>
+</head>
+<body>
+  <header>
+        <nav class="navbar">
+            <div class="logo">
+                <a href="index.php"><img src="../IMG/Logotipo.jpg" alt="logo_Adote_Fácil"></a>
+            </div>
+        <div class="dropdown">
+            <input type="checkbox" id="burger-menu">
+            <label class="burger" for="burger-menu">
+                <span></span>
+                <span></span>
+                <span></span>
+            </label>
+            <div class="dropdown-content">
+                <a href="../index.php">Início</a>
+                <a href="sobre.php">Sobre Nós</a>
+                <a href="adote.php" id="adote">Adote um pet</a>
+                <a href="comoajudar.php">Como ajudar</a>
+
+                <?php if (!isset($_SESSION['usuario_id'])): ?>
+                    <a href="Paginas/entrar.html" id="btn-entrar" class="botao-entrar">Entrar</a>
+                <?php else: ?>
+                    <div class="usuario-box" id="userMenu">
+                        <img src="IMG/usuario/<?php echo $_SESSION['usuario_foto']; ?>" 
+                            class="foto-perfil" alt="Foto">
+
+                        <div class="dropdown-user">
+                            <span class="nome-dropdown">
+                                <?php echo explode(" ", $_SESSION['usuario_nome'])[0]; ?>
+                            </span>
+
+                            <a href="PHP/Usuario/perfil.php">Perfil</a>
+                            <a href="PHP/Usuario/logout.php">Sair</a>
+                        </div>
+                    </div>
+                <?php endif; ?>
+            </div>
+        </div>
+        </nav>
+    </header>
+
+  <main>
+    <div class="boxpet">
+      <form action="../PHP/PETs/cadastrarPET.php" method="post" enctype="multipart/form-data">
+        <fieldset>
+          <legend><b>Cadastre seu Pet</b></legend>
+
+          <div class="inputBox">
+            <label for="nome" class="labelinput">Nome do Animal</label>
+            <input type="text" name="nome" id="nome" class="inputUser" required />
+          </div>
+
+          <div class="inputBox">
+            <label for="genero" class="labelinput">Informe o gênero do seu animal</label>
+            <select name="genero" id="genero" required>
+              <option value="">Selecione o gênero</option>
+              <option value="Macho">Macho</option>
+              <option value="Fêmea">Fêmea</option>
+            </select>
+          </div>
+
+            <div id="duo">
+            <div class="inputBox">
+            <label for="peso">Peso (Kg)</label>
+            <input type="number" name="peso" id="peso" class="inputUser" step="0.1" min="0"/>
+            </div>
+            
+            <div class="inputBox">
+            <label for="idade">Idade (Anos)</label>
+            <input type="number" name="idade" id="idade" class="inputUser" min="0"/>
+            </div>
+            </div>
+          
+            <div id="duo">
+          <div class="inputBox">
+            <label for="especie" class="labelinput">Espécie</label>
+            <input type="text" name="especie" id="especie" class="inputUser" required />
+          </div>
+          
+          <div class="inputBox">
+            <label for="raca" class="labelinput">Raça</label>
+            <input type="text" name="raca" id="raca" class="inputUser" required />
+          </div>
+          </div>
+          
+          <div id="duo">
+          <div class="inputBox">
+            <label for="porte" class="labelinput">Porte</label>
+            <select name="porte" id="porte">
+              <option value="pequeno">Pequeno</option>
+              <option value="medio">Médio</option>
+              <option value="grande">Grande</option>
+            </select>
+          </div>
+
+          <div class="inputBox">
+            <label for="situacao" class="labelinput">Situação</label>
+            <select name="situacao" id="situacao">
+              <option value="Nenhum">Nenhum</option>
+              <option value="Vacinado">Vacinado</option>
+              <option value="Vacinado e Castrado">Vacinado e Castrado</option>
+              <option value="Castrado">Castrado</option>
+            </select>
+          </div>
+          </div>
+
+          <div class="inputBox">
+            <label for="sobre" class="labelinput">Sobre o Pet</label>
+            <textarea name="sobre" id="sobre" class="inputUser" rows="4" placeholder="Conte um pouco sobre seu pet..."></textarea>
+          </div>
+
+          <div class="inputBox">
+            <label for="imagem" class="labelinput">Insira uma foto do Animal</label>
+            <input type="file" name="foto" id="foto" class="inputUser" accept="image/*" required>
+          </div>
+
+          <p>
+            Ao clicar no botão você estará automaticamente concordando com os nossos
+            <a href="#">Termo de Uso e Política de Privacidade</a> do site Adote Fácil
+          </p>
+
+          <button type="submit">Salvar</button>
+        </fieldset>
+      </form>
+    </div>
+  </main>
+
+    <footer>
+    <div class="footer-coluna" id="cl1">
+        <h2>Peludinhos do bem</h2>
+        <p>08989-8989898</p>
+        <p>Rua Santa Helena, 21, Parque Alvorada,<br> Imperatriz-MA, CEP 65919-505</p>
+        <p>adotefacil@peludinhosdobem.org</p>
+    </div>
+
+    <div class="footer-coluna" id="cl2">
+        <a href="sobre.html"></a>
+        <h2>Conheça a História da Peludinhos do Bem</h2>
+    </div>
+
+    <div class="footer-coluna" id="cl3">
+        <h2>Contatos</h2>
+
+        <div class="icons-row">
+            <a href="https://www.instagram.com/">
+            <img src="IMG/index/insta.png" alt="Instagram">
+            </a>
+
+            <a href="https://web.whatsapp.com/">
+            <img src="IMG/index/—Pngtree—whatsapp icon whatsapp logo whatsapp_3584845.png" alt="Whatsapp">
+            </a>
+        </div>
+    </div>
+
+
+    <div class="footer-rodape">
+        <p>Desenvolvido pela Turma-20 Tecnico de Informatica para Internet (Peludinhos do Bem). 2025 &copy;Todos os direitos reservados.</p>
+    </div>
+    
+    </footer>
+
+</body>
+</html>
